c647b923640049e111eff85f7e84fb9e64c75ac3
Lopezramosnicolas <54640509+Lopezramosnicolas@users.noreply.github.com>
2019-08-28 15:32:39 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+/*Para realizar comentarios en php de una sola linea 
+utilizamos //*/
+
+/*
+Para realizar un comentario de mas de una linea
+utilizamos: (barra asterisco, asterisco barra).
+*/
+
+/*
+php es el acronimo de HyperText Pre-processor
+o pre-prosesador de hipertexto el cual se ejecuta en un servidor 
+web estara en nuestra PC y cuando lo subamos a internet estara en
+ el donde alojaremos nuestro sitio, en nuestro caso 000.webhost.
+ Esta integrado con html  por lo que podemos utilizar mucha etiquetas del mismo
+*/
+echo "Hola Mundo";
+ ?>
